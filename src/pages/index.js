220cc839ec78f40aa5af4f807663ce5dc0d5c3ba
Ana Lúcia Medeiros <ana.medeiros@exploitcapital.com.br>
2022-07-30 11:34:12 -0300

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <meta property="og:title" content="WebChat" />
+        <meta property="og:description" content="O WebChat permite que você se conecte rapidamente à pessoas que possuem interesse no mesmo assunto que você!" />
+      </Head>
+      <main>
+        <h2>WebChat</h2>
+      </main>
+    </div>
+  )
+}
